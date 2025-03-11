d6b2d3039af2a143fe0ccf033955b2858964e91c
William Boles <wibosco@users.noreply.github.com>
2025-03-11 15:07:40 +0000

    Added initial project

---
+++ BackgroundTransferRevised-Example/BackgroundTransferRevised_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundTransferRevised_ExampleApp.swift
+//  BackgroundTransferRevised-Example
+//
+//  Created by William Boles on 11/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundTransferRevised_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
